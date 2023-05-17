28d2d1e3b7fccc9769520545e6c063890723c170
Nitesh <nitesh@expertise.com>
2023-05-17 11:38:12 +0530

    add custom logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     
-    const [text, setText] = useState('Enter text here')
+    const [text, setText] = useState('')
     const handleOnChange = (event) => {
         setText(event.target.value)
     }
@@ -10,14 +10,30 @@ export default function TextForm(props) {
         let newText = text.toUpperCase()
         setText(newText)
     }
+    const handleLowerCaseClick = () => {
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
     
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myText" rows="8"></textarea>
+        <>
+            <div>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myText" rows="8"></textarea>
+                </div>
+                <button onClick={handleUpperCaseClick} className="btn btn-primary mx-2">Convert to Uppercase</button>
+                <button onClick={handleLowerCaseClick} className="btn btn-primary mx-2">Convert to Lowercase</button>
             </div>
-            <button onClick={handleUpperCaseClick} className="btn btn-primary">Convert to Uppercase</button>
-        </div>
+            <div className="container my-3">
+                <h1>Your text summay</h1>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
+        
     )
 }
+
