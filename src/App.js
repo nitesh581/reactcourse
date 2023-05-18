471e85a6c186a56b7a7b4ad8a95faafd40afbf30
Nitesh <nitesh@expertise.com>
2023-05-18 11:59:47 +0530

    apply dark mode to whole application

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar'
@@ -5,11 +6,24 @@ import TextForm from './components/TextForm'
 import PropTypes from 'prop-types'
 
 function App() {
+
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () => {
+      if (mode === 'light') {
+          setMode('dark')
+          document.body.style.backgroundColor = '#042743'
+      } else {
+          setMode('light')
+          document.body.style.backgroundColor = 'white'
+      }
+  }
+
   return (
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <div className='container my-3'>
-        <TextForm heading="Enter the text to analyze below" />
+        <TextForm heading="Enter the text to analyze below" mode={mode} />
         {/* <About/> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,10 +29,20 @@ export default function TextForm(props) {
     
     return (
         <>
-            <div>
+            <div className='container' style={{color: props.mode === 'light' ? 'black' : 'white'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myText" rows="8"></textarea>
+                    <textarea 
+                        className="form-control" 
+                        value={text} 
+                        onChange={handleOnChange} 
+                        style={{
+                            color: props.mode === 'dark' ? 'white' : 'black',
+                            backgroundColor: props.mode === 'dark' ? 'grey' : 'white',
+                        }} 
+                        id="myText" 
+                        rows="8"
+                    ></textarea>
                 </div>
                 <button onClick={handleUpperCaseClick} className="btn btn-primary mx-2">Convert to Uppercase</button>
                 <button onClick={handleLowerCaseClick} className="btn btn-primary mx-2">Convert to Lowercase</button>
@@ -40,7 +50,7 @@ export default function TextForm(props) {
                 <button onClick={handleCopyTextClick} className="btn btn-primary mx-2">Copy Text</button>
                 <button onClick={handleExtraSpaces} className="btn btn-primary mx-2">Remove Extra Spaces</button>
             </div>
-            <div className="container my-3">
+            <div className="container my-3" style={{color: props.mode === 'light' ? 'black' : 'white'}}>
                 <h1>Your text summay</h1>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} minutes read</p>
